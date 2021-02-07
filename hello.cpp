e1a60ca5b686ee9c8181c5dd50e84a085ce0b744
hcruz-0 <65474490+hcruz-0@users.noreply.github.com>
2021-02-07 15:18:22 -0500

    Hello world commit with instructions
     Look at the code comment for instructions or contact scrum master for help

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World"<<endl;
+    //please include your name as a comment and add a cout statement!
+    // HI my name is Hazel
+    cout<<" Hope you had a great weekend"<<endl;//example cout statement
+}
